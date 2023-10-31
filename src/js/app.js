26449059f6610de14d1cf447db959c58518f6455
Yaroslav <Tregubov__1993@ukr.net>
2023-10-31 22:23:01 +0200

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,23 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// const swiper = new Swiper();
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    
+  });
